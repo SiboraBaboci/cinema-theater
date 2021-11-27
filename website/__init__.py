14e3413c3cf58e38a0cb46926dd1b3277baa9fc1
Sibora Baboci <borababoci1@gmail.com>
2021-11-27 18:37:03 +0100

    remote db connection

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,10 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__, template_folder='./template')
     app.config['SECRET_KEY'] = 'palma'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "mysql+mysqldb://22_appweb_02:V4croAJy@mysql.lab.it.uc3m.es/22_appweb_02a"
     db.init_app(app)
 
     from .views import views
@@ -36,5 +39,6 @@ def create_app():
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
+        db.drop_all(app=app)
         db.create_all(app=app)
         print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -1,10 +1,12 @@
 from enum import unique
+import enum
 from . import db 
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 #this is a one to many relationship (one user has many notes) 
 #so the foreign key shown below is only for one to many relationships.
+
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,11 +15,24 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
+class UserRole(enum.Enum):
+    customer = 1
+    manager = 2
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    role = db.Column(db.Enum(UserRole), nullable=False)
+    reservations = db.relationship(
+        "Reservation", backref="user",lazy=True)
     notes = db.relationship('Note')  #this tells flask and sqlalchemy 
     #that everytime we create a note, add into this user's notes relationship that note id. 
     #(this will be a list that will store all the different notes)
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
