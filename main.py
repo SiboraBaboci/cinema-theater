6a55245d72a07f019ff3d0dfc60a9912b695486f
Jon Hud <100468666@alumnos.uc3m.esgit clone git@gitlab.com:JonnHud/webapplications.gitgit config --global user.name Jon>
2021-11-30 16:46:17 +0100

    Inserted render functions for main & movie view

--- main.py
+++ main.py
@@ -2,5 +2,17 @@ from website import create_app
 
 app = create_app()
 
+#if we start the main page and nothing else, the main_view template is loaded into the skeleton
+@app.route("/")
+def open_main():
+    #location of the template might have to be specified further in order for the return statement to work
+    return render_template('main_view.html')
+
+@app.route("/movie")
+def open_movie():
+    return render_template('movie_view.html')
+
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
