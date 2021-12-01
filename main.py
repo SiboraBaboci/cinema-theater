4ffdb10730826ed86fb8ecfeff1c98e05ff7714a
Jon Hud <100468666@alumnos.uc3m.esgit clone git@gitlab.com:JonnHud/webapplications.gitgit config --global user.name Jon>
2021-12-01 17:48:24 +0100

    inserted login requirement, changed name to main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 app = create_app()
 
 #if we start the main page and nothing else, the main_view template is loaded into the skeleton
-@app.route("/main_movie")
+@app.route("/main")
 def open_main():
     #location of the template might have to be specified further in order for the return statement to work (maybe '/additional templates/main_view.html')
     return render_template('main_view.html', user=current_user)
@@ -16,10 +16,12 @@ def open_movie():
     return render_template('movie_view.html',  user=current_user)
 
 @app.route("/reservation",  methods=['GET', 'POST'])
+@login_required
 def open_reservation():
     return render_template('reservation_view.html',  user=current_user)
 
 @app.route("/customer",  methods=['GET', 'POST'])
+@login_required
 def open_customer():
     return render_template('customer_view.html',  user=current_user)
 
