244079631e6dd3a393942c59fd58c95aedb65b5c
OTRAS TITULACIONES <0470385@lab.it.uc3m.es>
2021-12-04 02:22:51 +0100

    get past/future movies and click on the movie for info

--- main.py
+++ main.py
@@ -10,21 +10,35 @@ app = create_app()
 #if we start the main page and nothing else, the main_view template is loaded into the skeleton
 @app.route("/main")
 def open_main():
-    projections = Projection.query.filter(func.DATE(Projection.date)<=date.today())
-    print(projections[0])
-    past_movie_id =[] 
-    for projection in projections:
-        past_movie_id.append(projection.movie_id)
+    past_projections = Projection.query.filter(func.DATE(Projection.date)<=date.today())
+    past_movie_ids =[] 
+    for projection in past_projections:
+        past_movie_ids.append(projection.movie_id)
+
+    past_movies = []  
+    for past_movie_id in past_movie_ids:
+        past_movies.append(Movie.query.get(past_movie_id))
+
+
+    future_projections = Projection.query.filter(func.DATE(Projection.date)>=date.today())
+    future_movie_ids =[] 
+    for projection in future_projections:
+        future_movie_ids.append(projection.movie_id)
+
+    future_movies = []  
+    for future_movie_id in future_movie_ids:
+        future_movies.append(Movie.query.get(future_movie_id))
+
     
-    past_movies = Movie.query.filter(Movie.id.in_(past_movie_id))
-    print(past_movies[0])
     #location of the template might have to be specified further in order for the return statement to work (maybe '/additional templates/main_view.html')
-    return render_template('main_view.html', user=current_user, movies = past_movies)
+    return render_template('main_view.html', user=current_user, past_movies = past_movies, future_movies = future_movies)
 
 #if we open the /movie page, the movie view is loaded into the skeleton
-@app.route("/movie",  methods=['GET', 'POST'])
-def open_movie():
-    return render_template('movie_view.html',  user=current_user)
+@app.route("/movie/<movie_id>",  methods=['GET', 'POST'])
+def open_movie(movie_id):
+    movie = Movie.query.filter_by(id = movie_id).first()
+    print(movie.img_str)
+    return render_template('movie_view.html',  user=current_user, movie = movie)
 
 @app.route("/reservation",  methods=['GET', 'POST'])
 @login_required
